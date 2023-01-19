744133ce78628b172cfee9dd5ad76c9603e1f681
Jasmine David <david.jasmine@student.greenriver.edu>
2023-01-18 20:25:22 -0500

    Controller and home page view added

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+// This is my controller
+
+// Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// Require the autoload file
+require_once('vendor/autoload.php');
+
+// Instantiate F3 base class
+$f3 = Base::instance();
+
+
+// Define a default route (328/diner)
+$f3->route('GET /', function (){ // invokes an anonymous function
+    // Instantiate view
+    $view = new Template();
+    echo $view->render('views/diner-home.html');
+//    echo "Welcome";
+});
+
+// Run Fat-Free
+$f3->run();
+
+?>
