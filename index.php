9d8d044ecf803caac299ab09ab173515c0a3599c
Jasmine David <david.jasmine@student.greenriver.edu>
2023-01-24 23:00:04 -0500

    Add order form and summary page

--- index.php
+++ index.php
@@ -6,6 +6,9 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+// Start a session
+session_start();
+
 // Require the autoload file
 require_once('vendor/autoload.php');
 
@@ -33,6 +36,31 @@ $f3->route('GET /lunch', function (){
     // Instantiate view
     $view = new Template();
     echo $view->render('views/lunch.html');
+});
+
+// Define a order form route (328/diner/order1)
+$f3->route('GET||POST /order1', function ($f3){
+
+    // if the form has been posted
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        // move data from POST array to SESSION array
+        $_SESSION['food'] = $_POST['food'];
+        $_SESSION['meal'] = $_POST['meal'];
+
+        //redirect to summary page
+        $f3->reroute('summary');
+
+    }
+    // Instantiate view
+    $view = new Template();
+    echo $view->render('views/order-form1.html');
+});
+
+// Define a order summary route (328/diner/summary)
+$f3->route('GET /summary', function (){
+    // Instantiate view
+    $view = new Template();
+    echo $view->render('views/summary.html');
 });
 
 // Run Fat-Free
